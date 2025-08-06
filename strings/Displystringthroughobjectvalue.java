8a3afc8edb5240ce3789b260b485e15a29c00f52
gyaneshwar <gyaneshwarkompally@gmail.com>
2025-08-06 14:29:44 +0530

    Add files via upload

---
+++ strings/Displystringthroughobjectvalue.java
@@ -0,0 +1,20 @@
+package strings;
+
+import java.util.*;
+
+
+public class Displystringthroughobjectvalue {
+	public static void main(String[] args) {
+		
+	System.out.print("Enter a string:");
+	
+    Scanner sc=new Scanner(System.in);
+    String i=sc.nextLine();
+    String message=new String(i);
+    
+    System.out.println("entered :" +message);
+    
+    
+    
+}
+}
---
+++ strings/Readstringvalueandprintontheconsole.java
@@ -0,0 +1,16 @@
+package strings;
+
+import java.util.*;
+
+public class Readstringvalueandprintontheconsole {
+
+	    public static void main(String[] args) {
+	    	Scanner sc=new Scanner(System.in);
+	        String message = "Hello, this is a string!";
+	        System.out.println(message);
+	    
+	
+
+	}
+
+}
